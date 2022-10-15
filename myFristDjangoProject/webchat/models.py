ce25082297856a63f01815e6422baaf37c5df422
Ijjaz Khan <ijazkhan19932@gmail.com>
2022-10-15 16:29:23 +0500

    models
    models

--- myFristDjangoProject/webchat/models.py
+++ myFristDjangoProject/webchat/models.py
@@ -1,3 +1,21 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class ChatBoard(models.Model):
+    name =models.CharField(max_length=35 , unique=True)
+    details= models.CharField(max_length=150)
+
+class ChatTopic(models.Model):
+    subject =models.CharField(max_length= 250)
+    lastUpdate =models.DateField(auto_now_add =True)
+    boardName = models.ForeignKey(ChatBoard,related_name='topics',on_delete=models.CASCADE)
+    bordStarter =models.ForeignKey(User,related_name ='topics',on_delete=models.CASCADE)
+
+class Post(models.Model):
+    message = models.TextField(max_length = 5000)
+    topic = models.ForeignKey(ChatTopic,related_name ='post',on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add = True)
+    updatedAt =models.DateTimeField(null=True)
+    createdBy =models.ForeignKey(User,related_name ='post',on_delete=models.CASCADE)
+    updatedBy = models.ForeignKey(User,null= True,related_name ='+',on_delete=models.CASCADE)
